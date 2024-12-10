5d607aead5446befae5576f1c827341bdcf2665a
sneha <sneha1423@protonmail.com>
2024-12-10 07:38:21 +0000

    port to pages and ignore .env file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Aside />
+      <Form />
+    </div>
+  );
+}
+
+export default Home;
